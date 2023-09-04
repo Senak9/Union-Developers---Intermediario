fababd927e6c8f346e9bc29112808a89bbfa5fcb
Senak9 <joaomarcelo.o.rocha@gmail.com>
2023-09-04 14:22:24 -0300

    feat: setup ReactQuerry

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
 import MainRoutes from "./routes";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MainRoutes />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <MainRoutes />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
