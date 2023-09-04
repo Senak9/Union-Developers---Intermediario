6f98c5c929877beb7d43140aad070ba64a8b1d22
Senak9 <joaomarcelo.o.rocha@gmail.com>
2023-09-04 10:14:23 -0300

    feat: setup styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
+import "./stlyes/index.css";
 import { BrowserRouter } from "react-router-dom";
 import MainRoutes from "./routes";
 
