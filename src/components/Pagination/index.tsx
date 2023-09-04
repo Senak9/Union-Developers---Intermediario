c75deb030a5a5e5fa55df1a5915370fe0b1868dc
Senak9 <joaomarcelo.o.rocha@gmail.com>
2023-09-04 20:10:27 -0300

    feat: configurando paginação

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,35 @@
+import "./styles/index.css";
+
+interface PaginationProps {
+  page: number;
+  total: number;
+  offset: number;
+  setOffset: (offset: number) => void;
+  setPage: (page: number) => void;
+}
+
+function Pagination({
+  page,
+  total,
+  offset,
+  setOffset,
+  setPage,
+}: PaginationProps) {
+  return (
+    <div className="container-pagination">
+      <button onClick={() => (page === 1 ? setPage(1) : setPage(page - 1))}>
+        {"<"}
+      </button>
+      <button onClick={() => setPage(1)}> 1 </button>
+      <button onClick={() => setPage(2)}> 2 </button>
+      <button onClick={() => setPage(3)}> 3 </button>
+      <button onClick={() => setPage(4)}> 4 </button>
+      <button onClick={() => setPage(5)}> 5 </button>
+      <button onClick={() => (page === 5 ? setPage(5) : setPage(page + 1))}>
+        {">"}
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,19 +4,29 @@ import "./styles/index.css";
 import api from "../../services/api";
 import { useQuery } from "react-query";
 import { useState } from "react";
+import Pagination from "../../components/Pagination";
 
 function List() {
   const [page, setPage] = useState<number>(1);
 
   const { data } = useQuery(["getUsers", page], () => {
-    return api.get(`/?page=${page}&results=10`);
+    return api.get(`/?page=${page}&results=10&seed=abc`);
   });
+
+  const [offset, setOffset] = useState<number>(0);
 
   console.log("olha aqui,", data?.data);
   return (
     <div>
       <SearchBar />
       {data && <UsersTable users={data?.data.results} />}
+      <Pagination
+        page={page}
+        total={120}
+        offset={offset}
+        setOffset={setOffset}
+        setPage={setPage}
+      />
     </div>
   );
 }
